354c29dab7c6c5f1fd1660614f3e653b7d94b6c3
johnsonlee <g.johnsonlee@gmail.com>
2026-04-09 14:34:30 +0900

    tmd: bump to 0.2.0 (TOC sidebar)

--- Formula/tmd.rb
+++ Formula/tmd.rb
@@ -1,8 +1,8 @@
 class Tmd < Formula
   desc "Terminal markdown previewer, powered by carbonyl"
   homepage "https://github.com/johnsonlee/tmd"
-  url "https://github.com/johnsonlee/tmd/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "bf00c8dc590a2bbfd8701b4511fb538650f78621dbeb9a2aac3c9b23354ac3e5"
+  url "https://github.com/johnsonlee/tmd/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "a619bc733ba8e10540bb24d6722e4e0f29cfaea719a18469f6ea36ba5d08cffb"
   license "MIT"
   head "https://github.com/johnsonlee/tmd.git", branch: "main"
 
